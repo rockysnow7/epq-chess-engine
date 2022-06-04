e9dc530d69862b5be7dcd701be6ef9760a65b72d
Finn Eyles <rockysnow7@gmail.com>
2022-06-04 23:01:45 +0100

    addded functions to measure elo and overall engine score

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use chess::Color;
 
 mod engine;
 mod play;
+mod measure;
 
 fn main() {
     let mut engine_1 = engine::Engine::new(0, engine::PruningType::None);
-    let mut engine_2 = engine::Engine::new(0, engine::PruningType::None);
+    let mut engine_2 = engine::Engine::new(0, engine::PruningType::AlphaBeta);
 
-    _ = play::play_engines(&mut engine_1, &mut engine_2, true);
+    let mean_time_per_move_1 = engine_1.measure_mean_nanos_per_move(100);
+    let mean_time_per_move_2 = engine_1.measure_mean_nanos_per_move(100);
+    let (elo_1, elo_2) = measure::measure_elo(&mut engine_1, &mut engine_2, 100);
+
+    let score_1 = measure::engine_score(elo_1, elo_2, mean_time_per_move_1, mean_time_per_move_2);
+
+    println!("{} {}", elo_1, elo_2);
 }
---
+++ src/measure.rs
@@ -0,0 +1,53 @@
+use crate::{
+    engine::Engine,
+    play::play_engines
+};
+use indicatif::ProgressBar;
+
+/// Plays two engines against each other for `num_games` games and returns the
+/// Elo score of each engine.
+pub fn measure_elo(engine_1: &mut Engine, engine_2: &mut Engine, num_games: u32) -> (u16, u16) {
+    let mut rating_1 = 1500f32;
+    let mut rating_2 = 1500f32;
+
+    let pb = ProgressBar::new(num_games as u64);
+    for i in 0..num_games {
+        let expected_1 = 1.0 / (1.0 + 10f32.powf((rating_2 - rating_1) as f32 / 400.0));
+        let expected_2 = 1.0 / (1.0 + 10f32.powf((rating_1 - rating_2) as f32 / 400.0));
+
+        if i % 2 == 0 {
+            let result = play_engines(engine_1, engine_2, false);
+            if result == 1 {
+                rating_1 += 32.0 * (1.0 - expected_1);
+                rating_2 += 32.0 * (0.0 - expected_2);
+            } else if result == -1 {
+                rating_1 += 32.0 * (0.0 - expected_1);
+                rating_2 += 32.0 * (1.0 - expected_2);
+            } else {
+                rating_1 += 32.0 * (0.5 - expected_1);
+                rating_2 += 32.0 * (0.5 - expected_2);
+            }
+        } else {
+            let result = play_engines(engine_2, engine_1, false);
+            if result == 1 {
+                rating_2 += 32.0 * (1.0 - expected_2);
+                rating_1 += 32.0 * (0.0 - expected_1);
+            } else if result == -1 {
+                rating_2 += 32.0 * (0.0 - expected_2);
+                rating_1 += 32.0 * (1.0 - expected_1);
+            } else {
+                rating_2 += 32.0 * (0.5 - expected_2);
+                rating_1 += 32.0 * (0.5 - expected_1);
+            }
+        }
+
+        pb.inc(1);
+    }
+    pb.finish();
+
+    (rating_1 as u16, rating_2 as u16)
+}
+
+pub fn engine_score(elo_old: f32, elo_new: f32, mean_time_per_move_old: f32, mean_time_per_move_new: f32) -> f32 {
+    (elo_new / elo_old) * (mean_time_per_move_old / mean_time_per_move_new)
+}
